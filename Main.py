845baff2982bfee0249bebeca634bcff7a404a6b
MrBr00d <42277715+MrBr00d@users.noreply.github.com>
2019-08-21 15:51:44 +0200

    Update Main.py
    Added support for solution function

--- Main.py
+++ Main.py
@@ -2,12 +2,15 @@ import calculator
 import os
 
 print('1 = Stock dilution')
-print('2 = NA')
+print('2 = Solution')
 choice = int(input("What action do you want to perform? "))
 
 if choice == 1:
   os.system('cls')
   calculator.dilution()
+elif choice == 2:
+  os.system('cls')
+  calculator.solution()
 else:
   print('No valid choice')
 input("Press enter to exit.")
