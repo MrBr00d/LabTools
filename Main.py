bf989864e4dd5a10b2478e8b7595133532a04caf
MrBr00d <42277715+MrBr00d@users.noreply.github.com>
2019-08-29 09:36:38 +0200

    Removed os module for mobile support

--- Main.py
+++ Main.py
@@ -1,5 +1,4 @@
 import calculator
-import os
 
 print('1 = Stock dilution')
 print('2 = Solution')
@@ -7,13 +6,10 @@ print('3 = (W%/V)')
 choice = int(input("What action do you want to perform? "))
 
 if choice == 1:
-  os.system('cls')
   calculator.dilution()
 elif choice == 2:
-  os.system('cls')
   calculator.solution()
 elif choice == 3:
-  os.system('cls')
   calculator.Weight_volume_percent()
 else:
   print('No valid choice')
