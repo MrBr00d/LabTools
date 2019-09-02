65dcac14812c6b2a6d3fbb0e0e6a7d1514670f46
MrBr00d <42277715+MrBr00d@users.noreply.github.com>
2019-09-02 20:21:25 +0200

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,25 +1,34 @@
 def dilution():
-  stock_concentration = float(input('Enter stock concentration here: ').replace(',', '.'))
-  desired_concentration = float(input('Enter desired concentration here: ').replace(',', '.'))
-  desired_volume = float(input('Enter desired volume here: ').replace(',', '.'))
-  dilution_factor = desired_concentration / stock_concentration
-  stock_calculation = dilution_factor * desired_volume
-  dilutant_calculation = desired_volume - stock_calculation
+  while True:
+    stock_concentration = float(input('Enter stock concentration here: ').replace(',', '.'))
+    if (stock_concentration == 0):
+      break
+    desired_concentration = float(input('Enter desired concentration here: ').replace(',', '.'))
+    desired_volume = float(input('Enter desired volume here: ').replace(',', '.'))
+    dilution_factor = desired_concentration / stock_concentration
+    stock_calculation = dilution_factor * desired_volume
+    dilutant_calculation = desired_volume - stock_calculation
 
-  print(f'Stock volume to be added: {stock_calculation}.')
-  print(f'Dilutant volume to be added: {dilutant_calculation}')
+    print(f'Stock volume to be added: {stock_calculation}.')
+    print(f'Dilutant volume to be added: {dilutant_calculation}')
 
 def solution():
-  desired_volume = float(input("Enter desired volume (L): "))
-  desired_concentration = float(input("Enter desired concentration (M): "))
-  molar_weight = float(input("Enter molar weight of substance (g/mol): "))
-  calculation = desired_volume * desired_concentration * molar_weight
-  print(f"Mass of substance to add: {calculation} g")
+  while True:
+    desired_volume = float(input("Enter desired volume (L): ").replace(',', '.'))
+    if desired_volume == 0:
+      break
+    desired_concentration = float(input("Enter desired concentration (M): ").replace(',', '.'))
+    molar_weight = float(input("Enter molar weight of substance (g/mol): ").replace(',', '.'))
+    calculation = desired_volume * desired_concentration * molar_weight
+    print(f"Mass of substance to add: {calculation} g")
 
 def Weight_volume_percent():
-  desired_wv = float(input("Desired % W/V :").replace(",", "."))
-  desired_volume = float(input("Desired volume (mL) :").replace(",", "."))
+  while True:
+    desired_wv = float(input("Desired % W/V :").replace(",", "."))
+    if desired_wv == 0:
+      break
+    desired_volume = float(input("Desired volume (mL) :").replace(",", "."))
   
-  calculation = desired_wv * desired_volume / 100
+    calculation = desired_wv * desired_volume / 100
   
-  print("Mass of solute needed (g): ", calculation, "g")
+    print("Mass of solute needed (g): ", calculation, "g")
