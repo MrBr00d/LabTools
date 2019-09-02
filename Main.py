c076c0d54d05fc1c6688b7eb35c2c77f2fc06a62
MrBr00d <42277715+MrBr00d@users.noreply.github.com>
2019-09-02 20:20:25 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,20 +1,36 @@
 import calculator
 import os
 
-print('1 = Stock dilution')
-print('2 = Solution')
-print('3 = (W%/V)')
-choice = int(input("What action do you want to perform? "))
+def menu():
+  print('1 = Stock dilution')
+  print('2 = Solution')
+  print('3 = (W%/V)')
+  choice = int(input("What action do you want to perform? "))
 
-if choice == 1:
-  os.system('cls')
-  calculator.dilution()
-elif choice == 2:
-  os.system('cls')
-  calculator.solution()
-elif choice == 3:
-  os.system('cls')
-  calculator.Weight_volume_percent()
-else:
-  print('No valid choice')
-input("Press enter to exit.")
+  if choice == 1:
+    os.system('cls')
+    calculator.dilution()
+  elif choice == 2:
+    os.system('cls')
+    calculator.solution()
+  elif choice == 3:
+    os.system('cls')
+    calculator.Weight_volume_percent()
+  else:
+    print('No valid choice')
+  again()
+
+def again():
+  print("Do you want to run the calculator again?")
+  print('1 = Yes')
+  print('2 = No')
+
+  choice = int(input())
+
+  if choice == 1:
+    os.system('cls')
+    menu()
+  else:
+    pass
+
+menu()
